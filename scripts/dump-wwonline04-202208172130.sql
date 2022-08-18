20e9c3976d73a2c9917c2e3862b5feb736946401
Gustavsds <detonagg14@gmail.com>
2022-08-17 21:43:26 -0300

    Adiconado dump do bnaco de dados vazio

---
+++ scripts/dump-wwonline04-202208172130.sql
@@ -0,0 +1,142 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: mysql.wwonline.com.br    Database: wwonline04
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.2.36-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aluno` (
+  `matricula` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aluno`
+--
+
+LOCK TABLES `aluno` WRITE;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `endereço`
+--
+
+DROP TABLE IF EXISTS `endereço`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `endereço` (
+  `id_endereço` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_endereço`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `endereço`
+--
+
+LOCK TABLES `endereço` WRITE;
+/*!40000 ALTER TABLE `endereço` DISABLE KEYS */;
+/*!40000 ALTER TABLE `endereço` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `filho`
+--
+
+DROP TABLE IF EXISTS `filho`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filho` (
+  `id_filho` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_filho`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filho`
+--
+
+LOCK TABLES `filho` WRITE;
+/*!40000 ALTER TABLE `filho` DISABLE KEYS */;
+/*!40000 ALTER TABLE `filho` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `municipio`
+--
+
+DROP TABLE IF EXISTS `municipio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `municipio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `municipio`
+--
+
+LOCK TABLES `municipio` WRITE;
+/*!40000 ALTER TABLE `municipio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `municipio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uf`
+--
+
+DROP TABLE IF EXISTS `uf`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `uf` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uf`
+--
+
+LOCK TABLES `uf` WRITE;
+/*!40000 ALTER TABLE `uf` DISABLE KEYS */;
+/*!40000 ALTER TABLE `uf` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'wwonline04'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-17 21:30:28
