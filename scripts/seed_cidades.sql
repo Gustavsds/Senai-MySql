175d7645db3bb25b1798ae5bb790610251bb6021
Gustavsds <110433665+Gustavsds@users.noreply.github.com>
2022-08-19 19:16:14 -0300

    Adição do scripts de cidades

---
+++ scripts/seed_cidades.sql
@@ -0,0 +1,11 @@
+INSERT into municipio (nome) values
+('LINHARES'),
+('VILA VELHA'),
+('SÃO MATHEUS'),
+('COLATINA'),
+('CONCEIÇÃO DO CASTELO'),
+('SANTA TEREZA'),
+('RIO BANANAL'),
+('VILA VALERIO'),
+('VITORIA'),
+('SOORETAMA')
