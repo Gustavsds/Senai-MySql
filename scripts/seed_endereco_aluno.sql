aeb0f464b328f2f1a997267172c4f6d1b4a1334b
Gustavsds <110433665+Gustavsds@users.noreply.github.com>
2022-08-23 19:25:45 -0300

    adição endereço

---
+++ scripts/seed_endereco_aluno.sql
@@ -0,0 +1,3 @@
+INSERT INTO endereco (numero, bairro, cep, logradouro, uf_id, municipio_id) values
+('1894', 'interlagos', '29.906-057', 'AV tome de souza', 42, 1),
+('1897', 'interlagos', '29.906-058', 'AV tome de souza', 42, 1)
