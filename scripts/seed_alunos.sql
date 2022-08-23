ef75ce1b2186b15e335b3a8d95d6457ad0c27314
Gustavsds <110433665+Gustavsds@users.noreply.github.com>
2022-08-23 19:26:25 -0300

    adição de alunos

---
+++ scripts/seed_alunos.sql
@@ -0,0 +1,3 @@
+INSERT into aluno (matricula, nome, cpf, telefone, email, data_nascimento, nome_mae, nome_pai, endereco_id) values
+('247789', 'Carlos Roberto connor', '156.789.456-57', '(27) 99875-4567', 'carlao@gmail.com', '2000:02:05', 'Maria aparecida connor', 'Ricardo connor', 1),
+('225874', 'Emanuel Batista', '146.057.134-78', '(27) 9999-5487', 'manuelll@gmail.com', '2002:06:06', 'Carla Batista', 'Osvaldo batista', 2)
